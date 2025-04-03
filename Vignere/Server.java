9675b867187e203dfb8be7205d19b6f6fd24a19c
jainavni08 <jain.avni.ind@gmail.com>
2025-04-03 23:12:43 +0530

    Vignere Cipher Final Code

--- Vignere/Server.java
+++ Vignere/Server.java
@@ -5,7 +5,9 @@ import java.net.*;
 public class Server {
     public static void main(String []args){
         try(ServerSocket ss = new ServerSocket(5000)){
+            System.out.println("Server ready");
             Socket socket = ss.accept();
+            System.out.println("Client connected");
             BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             PrintWriter output = new PrintWriter(socket.getOutputStream(),true);
             
